ab3c6af53b322a04ef69d4e13569d9dfa6c85c95
zainab rasheed <zainab-rasheed@netixsol.com>
2024-05-03 14:04:08 +0500

    changes to make code more readable

--- components/Footer.jsx
+++ components/Footer.jsx
@@ -1,18 +1,42 @@
 import Image from "next/image"
 import linkedinIcon from '../public/linkedin-icon.jpg'
+import Link from "next/link"
 const Footer = () => {
+  const lis = [
+    {
+      title:"HOME",
+      link:""
+    },
+    {
+      title:"PROCESS",
+      link:""
+    },
+    {
+      title:"WORK",
+      link:""
+    },
+    {
+      title:"FAQS",
+      link:""
+    },
+    {
+      title:"ABOUT US",
+      link:""
+    },
+    {
+      title:"PRIVACY",
+      link:""
+    },
+  ]
   return (
     <>
     <footer className="p-10 pt-20 bg-black font-spaceGrotesk">
         <div className="flex justify-between md:flex-col">
         <div>
         <ul className="flex flex-wrap gap-5 text-white text-sm sm:text-xs">
-            <li className="list-item-with-slash cursor-pointer">HOME</li>
-            <li className="list-item-with-slash cursor-pointer">PROCESS</li>
-            <li className="list-item-with-slash cursor-pointer">WORK</li>
-            <li className="list-item-with-slash cursor-pointer">FAQS</li>
-            <li className="list-item-with-slash cursor-pointer">ABOUT US</li>
-            <li className="cursor-pointer">PRIVACY</li>
+          {lis.map((li)=>(
+            <Link href={li.link}><li className="list-item-with-slash cursor-pointer">{li.title}</li></Link>
+          ))}
         </ul>
         <div className="text-white my-5 text-sm font-spaceGrotesk">
         <p className="sm:text-xs">© 2021 SVZ Design, All Rights Reserved</p>
--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -3,6 +3,27 @@ import React,{useState, useEffect} from 'react'
 
 import Logo from '../public/logo.png'
 import Image from 'next/image';
+import {MenuIcon, CrossIcon} from './svg'
+import Link from 'next/link';
+
+const lis = [
+  {
+    title:"Products",
+    link:""
+  },
+  {
+    title:"Subscription",
+    link:""
+  },
+  {
+    title:"Why Nura?",
+    link:"" 
+  },
+  {
+    title:"Support",
+    link:""
+  }
+]
 
 const Navbar = () => {
   const [showText, setShowText] = useState('');
@@ -19,11 +40,12 @@ const Navbar = () => {
       }
     };
     window.addEventListener("scroll", handleScroll);
-    // Clean up the event listener on component unmount
     return () => {
       window.removeEventListener("scroll", handleScroll);
     };
   }, []);
+
+  
 
   return (
     <>
@@ -32,24 +54,15 @@ const Navbar = () => {
         } xl:h-[90px]`}>
     <div><Image className='mx-10' src={Logo}></Image></div>
     <button className='hidden lg:block' onClick={onClick}>
-    {showText?  
-    
-         <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 absolute right-6 top-6">
-        <path strokeLinecap="round" strokeLinejoin="round" d="M6 18 18 6M6 6l12 12" />
-      </svg> : <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" className="w-10 h-10 absolute right-6 top-6">
-      <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
-    </svg>
-      }
-   
+    {showText? <MenuIcon/> : <CrossIcon/> }
 
     </button>
   <div className='2xl:block lg:hidden w-full relative'>
           <div>
             <ul className='flex gap-7 md:gap-3 items-center w-full'>
-                <li>Products</li>
-                <li>Subscription</li>
-                <li>Why Nura?</li>
-                <li>Support</li>
+                {lis.map((li)=>(
+                  <Link href={li.link}><li>{li.title}</li></Link>
+                ))}
                 <li className="absolute right-0"><button className=' bg-custom-green p-2 px-5 rounded-lg font-medium'>Cart (0)</button></li>
             </ul>
         </div>
@@ -66,13 +79,12 @@ function Text(){
       <>
         <div className='side_bar lg:block bg-black text-white top-20 right-0 absolute p-5 h-auto w-1/3 md:w-1/2 sm:w-[80%] z-50 rounded-lg'>
         <div>
-        <ul className=''>
-                <li className='py-2'>Products</li>
-                <li className='py-2'>Subscription</li>
-                <li className='py-2'>Why Nura?</li>
-                <li className='py-2'>Support</li>
-                <li className="py-2"><button className=' bg-custom-green p-2 px-5 rounded-lg font-medium'>Cart (0)</button></li>
-            </ul>
+        <ul>
+          {lis.map((li)=>(
+            <Link href={li.link}><li className='py-2'>{li.title}</li></Link>
+          ))}
+          <li className="py-2"><button className=' bg-custom-green p-2 px-5 rounded-lg font-medium'>Cart (0)</button></li>
+        </ul>
         </div>
         </div>
       </>
